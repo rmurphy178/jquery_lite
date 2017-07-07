26d4a6cb1e50dd0b7b33c77ed7d950645266435a
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-07-06 17:54:41 -0700

    finish append

--- jquery_lite/lib/jquery_lite.js
+++ jquery_lite/lib/jquery_lite.js
@@ -104,6 +104,32 @@ class DOMNodeCollection {
   empty() {
       this.html("");
   }
+  append(arg){
+    if (typeof arg === 'string') {
+      for (let i = this.selector.length - 1; i >= 0; i--) {
+        this.selector[i].innerHTML += arg;
+      }
+    } else if (arg instanceof HTMLElement) {
+      for (let i = this.selector.length - 1; i >= 0; i--) {
+        this.selector[i].innerHTML += arg.outerHTML;
+      }
+    } else {
+      this.each(node => {
+        // The argument to cloneNode indicates whether or not
+        // all children should be cloned.
+        arg.each(childNode => {
+          node.appendChild(childNode.cloneNode(true));
+        });
+      });
+
+      }
+    }
+
+  each(cb) {
+   // Our each passes in the node and index in traditional 'forEach' order,
+   // jquery passes in index first and binds the call to the element.
+   this.nodes.forEach(cb);
+ }
 }
 
 
